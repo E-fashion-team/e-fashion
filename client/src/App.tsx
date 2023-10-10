dd9e08003893520f800eaf2c72c04355fddd2d59
zeinebkhemida <zeineb.khemida11@gmail.com>
2023-10-10 15:35:21 +0100

    edit profile

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,41 @@
-
+import React,{useState}  from 'react';
 import './App.css';
 import NavBar from './components/NavBar';
 import Footer from './components/Footer';
-
-
+import EditProfile from "./components/editProfile/editProfile"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import axios from 'axios'
 
 function App() {
 
+
+// const updateUser=(name:string,newUser:object)=>{
+
+// axios.put(`http://localhost:5000/api/user/${name}`,newUser).then((response)=>{
+
+
+// }).catch((error)=>{
+// console.log(error)
+// })
+// }
+
+
   return (
-    <div className="App">
- <NavBar/>
- <Footer/>
-    </div>
+
+        <BrowserRouter>
+       
+
+      <Routes>
+        <Route path="/edit" element={<EditProfile />}>
+       
+       
+        </Route>
+      </Routes>
+  
+    </BrowserRouter>
+
+
+  
   );
 }
 
