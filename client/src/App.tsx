c74261f1749a69c69d5f2d87ba9c14eb6481ff95
zeinebkhemida <zeineb.khemida11@gmail.com>
2023-10-10 11:09:36 +0100

    store redux

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,15 @@
-import React from 'react';
 
 import './App.css';
 
+
+
 function App() {
+
   return (
-    <div className="App">
- hello fashions
-    </div>
+
+    <div>
+    hello
+  </div>
   );
 }
 
