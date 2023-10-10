c30450c9810bfaec608c55ecfafac20fc125faab
yjandoubi95 <58791201+yjandoubi95@users.noreply.github.com>
2023-10-10 09:13:54 +0100

    NAvBar&Footer

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 
 import './App.css';
+import NavBar from './components/NavBar';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
- hello fashions
+ <NavBar/>
+ <Footer/>
     </div>
   );
 }
