fefe26a1eca8401e28b77712c6609bdf98d1ea4a
yjandoubi95 <58791201+yjandoubi95@users.noreply.github.com>
2023-10-10 11:52:25 +0100

    Routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,23 @@
 import React from 'react';
 
+
 import './App.css';
 import NavBar from './components/NavBar';
 import Footer from './components/Footer';
+import {Routes,Route} from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
- <NavBar/>
- <Footer/>
+      <NavBar/>
+   <Routes>
+{/* <Route path='/' element={</>}/>
+<Route path='/' element={</>}/>
+<Route path='/' element={</>}/>
+<Route path='/' element={</>}/>
+<Route path='/' element={</>}/> */}
+   </Routes>
+   <Footer/>
     </div>
   );
 }
