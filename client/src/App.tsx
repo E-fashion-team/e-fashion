054fb0fa85085624db460d0b47a018c329bf1ed4
yjandoubi95 <58791201+yjandoubi95@users.noreply.github.com>
2023-10-11 14:33:16 +0100

    client card

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,23 +4,17 @@ import React,{useState}  from 'react';
 import './App.css';
 import NavBar from './components/NavBar';
 import Footer from './components/Footer';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-import axios from 'axios'
-import ExplorePage from "./components/explorePqge/explore"
-import Home  from "./pages/Home"
-import EditProfile from './components/editProfile/editProfile'
-import About from './components/About'
-import {CreatorStudioPage} from './components/CreatorStudio'
-import Market from "./components/market/market"
-
-
+import { Routes, Route } from "react-router-dom";
+import About from './components/About';
+import { CreatorStudioPage } from './components/CreatorStudio';
+import Home from './pages/Home';
 import { Stats } from './components/Statistique';
+import { Market } from './components/Market';
 
-
-
+import ExplorePage from './components/explorePqge/explore';
+import EditProfile from './components/editProfile/editProfile';
 import Drops from './components/Drops';
-
+import ClientCard from './components/ClientCard';
 function App() {
 
 
@@ -29,10 +23,8 @@ function App() {
   return (
 <div>
   
-      <NavBar/>
+      {/* <NavBar/> */}
   <Routes>
-   
-
   <Route path="/market"      element={<Market/>}/> 
   <Route path="/home"      element={<Home/>}/> 
 <Route  path="/explore"  element={<ExplorePage/>}  />
@@ -41,10 +33,9 @@ function App() {
 <Route path="/studio"   element={< CreatorStudioPage/>} />
 <Route path='/stats' element={<Stats />}/>
 <Route path='/drops' element={<Drops />}/>
-
-
+<Route path='/clientCard' element={<ClientCard />}/>
   </Routes> 
-   <Footer/>
+   {/* <Footer/> */}
  </div>
 
   );
