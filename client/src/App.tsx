e0bdc2d86a3a88cf865af979120a9849ac08e2d1
LouhichiMedSelim <lmedselim@gmail.com>
2023-10-11 10:15:47 +0100

    market

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,22 +7,27 @@ import Footer from './components/Footer';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import About from './components/About';
 import { CreatorStudioPage } from './components/CreatorStudio';
+import Home from './pages/Home';
+import { Stats } from './components/Statistique';
+import { Market } from './components/Market';
+
 function App() {
   return (
-
-    <BrowserRouter   >
+<div>
+  
       <NavBar/>
    
    
   <Routes>
 <Route path='/about' element={<About/>}/>
 <Route path='/creatorStudio' element={<CreatorStudioPage/>}/>
-{/* <Route path='/' element={</>}/>
-<Route path='/' element={</>}/>
-<Route path='/' element={</>}/> */}
+<Route path='/' element={<Home/>}/>
+<Route path='/stats' element={<Stats/>}/>
+<Route path='/market' element={<Market/>}/>
    </Routes>
  <Footer/>
-    </div>
+ 
+  </div> 
 
   );
 }
