fefd1151618b1c89219faad946405ecb107c1e01
LouhichiMedSelim <lmedselim@gmail.com>
2023-10-12 23:35:08 +0100

    video

--- server/index.js
+++ server/index.js
@@ -1,39 +1,41 @@
-const express=require('express')
-const app=express()
-const cors=require("cors")
-const http = require('http')
-const socketIo = require('socket.io')
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const http = require('http');
+const socketIo = require('socket.io');
+const cookieParser = require('cookie-parser'); 
+const Proxy = require('http-proxy').createProxyServer();
+const path = require('path');
+const config = require(path.join(__dirname, '../config/global.json'));
+
 const server = http.createServer(app);
-const io = socketIo(server,{cors:{origin:"http://localhost:3000"}});
-require("./models/model")
+const io = socketIo(server, { cors: { origin: 'http://localhost:3000' } });
 
-const port=5000
-const bodyparser = require("body-parser");
-const jwt = require("jsonwebtoken");
-var cookieParser = require("cookie-parser");
-require("dotenv").config();
+require('./models/model');
 
-app.use(express.json())
+const ProxyServer = 'http://localhost:' + config.Proxy.settings.port;
+const port = 5000;
 
-
-app.use(bodyparser.urlencoded({ extended: true }));
-app.use(cookieParser());
+app.use(express.json());
 app.use(cors());
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
-// app.use(cors())
-const routerMessages=require("./routers/messegeRouter.js")
-const routerUsers=require("./routers/userRouter.js")
-const routerProduct=require("./routers/productRouter.js")
-const routerBrand=require("./routers/brandRouter.js")
-const routerRoom=require("./routers/roomRouter.js")
-app.use("/api/message",routerMessages)
-app.use("/api/product",routerProduct)
-app.use("/api/user",routerUsers)
-// app.use("/api/room",routerRoom)
-app.use("/api/brand",routerBrand)
+// Socket.IO configuration
+const oi = socketIo(config.Server.settings.socket, {
+  cors: {
+    origin: 'http://localhost:3000',
+  },
+  // Additional socket.io configuration here
+});
 
-io.on('connection', (socket) => {
+oi.on('connection', (socket) => {
   console.log('A user connected');
+
+  socket.on('stream', (data) => {
+    socket.broadcast.emit('stream', data);
+  });
 
   socket.on('chat message', (msg) => {
     io.emit('chat message', msg);
@@ -44,27 +46,24 @@ io.on('connection', (socket) => {
   });
 });
 
+// Proxy HTTP requests
+app.all('/*', function (req, res) {
+  Proxy.web(req, res, { target: ProxyServer });
+});
 
+// API routes
+const routerMessages = require('./routers/messegeRouter.js');
+const routerUsers = require('./routers/userRouter.js');
+const routerProduct = require('./routers/productRouter.js');
+const routerBrand = require('./routers/brandRouter.js');
+const routerRoom = require('./routers/roomRouter.js');
 
+app.use('/api/message', routerMessages);
+app.use('/api/product', routerProduct);
+app.use('/api/user', routerUsers);
+app.use('/api/brand', routerBrand);
+// app.use('/api/room', routerRoom);
 
-// app.get('/api', (req, res) => {
-//     res.json({ message: 'This is your API!' });
-//   });
-
-  
-  // io.on('connection', (socket) => {
-  //   console.log('A user connected');
-  
-  //   socket.on('disconnect', () => {
-  //     console.log('User disconnected');
-  //   });
-  // })
-
-
-server.listen(port,()=>{
-
-console.log("backend running in port",port)
-
-})
-
-
+server.listen(port, () => {
+  console.log('Backend is running on port', port);
+});
