d4056d92130aeb35148bfc8518652a52bec904b1
zeinebkhemida <zeineb.khemida11@gmail.com>
2023-10-12 20:39:13 +0100

    chat

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -4,10 +4,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from './App';
 import { store } from './store/index'
 import { Provider } from 'react-redux'
-<<<<<<< HEAD
-=======
-
->>>>>>> 0fc36c0849cffa19dd99862b0f82c10fd568cfcf
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
@@ -18,16 +14,10 @@ root.render(
   <Provider store={store}>
     <App />
     </Provider>
-<<<<<<< HEAD
-  </BrowserRouter>
-
-);
-=======
    </BrowserRouter>
 
 
 )
 
->>>>>>> 0fc36c0849cffa19dd99862b0f82c10fd568cfcf
 
 
