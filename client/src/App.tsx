5e7e1510f33a4df4e64cf71344ffe5723f08eade
yjandoubi95 <58791201+yjandoubi95@users.noreply.github.com>
2023-10-12 13:30:04 +0100

    f-shows2

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,67 +1,50 @@
-import SignIn from './components/SignIn';
-import './App.css';
-import SignUp from './components/SignUp';
-import React,{useState}  from 'react';
-import './App.css';
-import NavBar from './components/NavBar';
-import Footer from './components/Footer';
-
+import SignIn from "./components/SignIn";
+import "./App.css";
+import SignUp from "./components/SignUp";
+import React, { useState } from "react";
+import "./App.css";
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import axios from 'axios'
+import axios from "axios";
 
-import About from './components/About';
-import { CreatorStudioPage } from './components/CreatorStudio';
-import Home from './pages/Home';
-import { Stats } from './components/Statistique';
+import About from "./components/About";
+import { CreatorStudioPage } from "./components/CreatorStudio";
+import Home from "./pages/Home";
+import { Stats } from "./components/Statistique";
 
+import FashionShow from "./components/fashion show/fashionShow";
 
-import FashionShow from "./components/fashion show/fashionShow"
+import { Market } from "./components/Market";
+import "./App.css";
+import ExplorePage from "./components/explorePqge/explore";
+import EditProfile from "./components/editProfile/editProfile";
 
-import { Market } from './components/Market';
-import './App.css'
-import ExplorePage from './components/explorePqge/explore';
-import EditProfile from './components/editProfile/editProfile';
-
-import Drops from './components/Drops';
-import ClientCard from './components/ClientCard';
-import FashionShows from './components/FashionShows';
-
-
-
+import Drops from "./components/Drops";
+import ClientCard from "./components/ClientCard";
+import FashionShows from "./components/FashionShows";
 
 function App() {
-
   return (
-<div>
+    <div>
+      <Routes>
+        <Route path="/market" element={<Market />} />
+        <Route path="/explore" element={<ExplorePage />} />
+        <Route path="/edit" element={<EditProfile />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/studio" element={<CreatorStudioPage />} />
+        <Route path="/stats" element={<Stats />} />
+        <Route path="/drops" element={<Drops />} />
+        <Route path="/clientCard" element={<ClientCard />} />
+        <Route path="/fashionShows" element={<FashionShows />} />
+        <Route path="/signUp" element={<SignUp />} />
+        <Route path="/signIn" element={<SignIn />} />
 
-  
-
-  <Routes>
-  <Route path="/market"      element={<Market/>}/> 
-<Route  path="/explore"  element={<ExplorePage/>}  />
- <Route path="/edit" element={< EditProfile/>}/> 
-<Route path="/about" element={< About/>}/>
-<Route path="/studio"   element={< CreatorStudioPage/>} />
-<Route path='/stats' element={<Stats />}/>
-<Route path='/drops' element={<Drops />}/>
-<Route path='/clientCard' element={<ClientCard />}/>
-<Route path='/fashionShows' element={<FashionShows />}/>
-    <Route path="/signUp"      element={<SignUp/>}/> 
-    <Route path="/signIn"      element={<SignIn/>}/> 
-     
-    <Route path="/"      element={<Home/>}/> 
-   
-
-
-
-  </Routes> 
-   {/* <Footer/> */}
-
- </div>
-
-
-
+        <Route path="/" element={<Home />} />
+      </Routes>
+      {/* <Footer/> */}
+    </div>
   );
 }
 
